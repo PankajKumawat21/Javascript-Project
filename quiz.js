18e1861c3711474c5331d082f12356131d793576
PankajKumawat21 <kumawatpankaj8152gmail.com>
2023-12-09 15:27:13 +0530

    javascript project

---
+++ quiz.js
@@ -0,0 +1,2 @@
+const answerEle = document.querySelectorAll(".answer")
+const [qucationElm, option_1 ,option_2 ,option_3, option_4] =document.querySelector("#qucation .option_1 .option_2 .option_3 .option_4")
